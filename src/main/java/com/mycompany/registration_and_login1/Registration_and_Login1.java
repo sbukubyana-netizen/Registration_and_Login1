169497be8151887b5618f1257ffdbc8fed8800fb
Sibusiso Kubyana <your-email@example.com>
2026-03-28 13:11:23 +0200

    First commit

---
+++ src/main/java/com/mycompany/registration_and_login1/Registration_and_Login1.java
@@ -0,0 +1,165 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registration_and_login1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Registration_and_Login1 {
+   
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        
+        //Declaring variables for global use
+        String username = "";
+        String password = "";
+        String cellnumber = "";
+        
+        
+        
+         while (true) { //creating a loop until conditions for username are met 
+        System.out.println("Enter your username");
+        username= scan.nextLine();
+        
+        if (username.length() <=5 && username.contains("_")) { //equal to or less than 5 characters and contains underscore
+            System.out.println("Username successfully captured");
+            break; // break loop
+         } 
+         
+        else {
+ System.out.println("Username is not correctly formartted; please ensure that username contains an underscore and is no more than five characters in length");}
+         }
+                
+        
+while (true) { //Loop until conditions for password are met
+System.out.println("Enter your new password");
+password = scan.nextLine();
+
+String regex_password = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[\\W_]).{8,}$"; // Declaring and assigning regex
+
+if (password.length() >=8 && password.matches(regex_password)) {
+     /** ^,it means the beginning of the string
+         (?=.*[A-Z], it scans for at least one uppercase
+         (?=.*[0-9]), it scans for at least one number
+         (?=.*[\\W_]), it scans for at least one special character
+          {8,} , it means at least 8 characters long
+         +$, means end of the string, containing one or more previous groups */ 
+     System.out.println("Password successfully captured");
+break;} //break loop
+   else {
+System.out.println("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character");
+}  
+}
+
+while(true) { //Loop until it meets the required conditions for cellphone
+  System.out.println("Enter your cell phone number");
+cellnumber= scan.nextLine();
+
+String regex = "^\\+[0-9]{1,3}[0-9]{1,15}$"; //Declaring and assigning regex
+
+
+if (cellnumber.matches(regex)) {
+    /** Regex explanation :
+^\+ means the cell number will start with a +
+[0-9] country code, {1,3} only 1 to 3 digits allowed
+[0-9] number parts, {1,15} only 1 to 15 digits allowed
+$, means the last part must end with the last group*/
+    
+System.out.println("Cell phone number successfully added");
+break;} 
+
+else { 
+System.out.println("Cell phone number incorrectly formatted or does not contain international code");
+}
+}
+
+    
+//Login Process with a loop
+while (true) {
+        System.out.println("Enter your username to login");
+        String login_username= scan.nextLine();
+        
+        System.out.println("Enter your password to login");
+        String login_password= scan.nextLine();
+        
+         
+        if (login_username.equals( username) && (login_password.equals(password) ) ) { //"=" if i was using numbers
+    
+            System.out.println("Welcome" +" "+username+" "+"it is great to see you again");
+        break;} // break loop
+            
+        else {
+            System.out.println("Username or password incorrect please try again");
+            }
+           } 
+          
+   class login { // checking if the user meets certain conditions before granting access
+
+       
+       boolean checkUserName(String username) {
+           if ((username.length() <=5 && username.contains("_"))) {
+               return true;
+           } else {
+               return false;
+           }
+       }
+       
+    boolean checkPasswordComplexity(String password) {
+        if (password.length() >=8 && password.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[\\W_]).{8,}$")) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    boolean checkCellPhoneNumber (String cellnumber) {
+        if (cellnumber.matches("^\\+[0-9]{1,3}[0-9]{1,15}$")) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+        String registerUser(String username, String password) {
+        String message= "";
+        
+        
+        if (!(username.length() <=5 && username.contains("_")))// "!", means if length is NOT valid
+        {
+            message+= "The username is incorrectly formatted";   // "+= means add and assign        
+        }
+        if (! (password.length() >=8 && password.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[\\W_]).{8,}$"))) {
+      
+          message+="The password does not meet the complexity requirements";
+            }
+        if (message.isEmpty()) {
+            message = "The above conditions have been met, and the user has been registered successfully";
+        }
+           return message;
+    }
+    boolean loginUser (String login_username, String username, String login_password, String password) {
+        if (login_username.equals( username) && (login_password.equals(password) )) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+        String returnLoginStatus (String login_username, String username, String login_password, String password) {
+        String message ="";
+        
+        if (!login_username.equals( username) && (login_password.equals(password) ) ) {
+            
+        message+="A failed login"; 
+        } else {
+            message+="A successful login";
+        }
+            
+       return message;
+        }
+   }
+    }
+}
